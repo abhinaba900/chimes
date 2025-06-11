dab3601016e99abb63f7041e50f3cf3acd177cc5
Abhinaba Jana <abhinabajana900@gmail.com>
2025-06-11 14:51:43 +0530

    vercel analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AuthContextProvider from "./AuthContext/AuthContext";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
+      <Analytics />
       <Component {...pageProps} />
     </AuthContextProvider>
   );
