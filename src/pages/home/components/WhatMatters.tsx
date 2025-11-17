c1f80a237219394a8a2c3a0048ef4dde44f1d249
Abhinaba Jana <abhinabajana900@gmail.com>
2025-11-17 17:27:29 +0530

    feat: add left-and-right padding to map container in WhatMatters component; adjust line-height and margins in global styles for improved layout

--- src/pages/home/components/WhatMatters.tsx
+++ src/pages/home/components/WhatMatters.tsx
@@ -72,7 +72,13 @@ export default function WhatMatters() {
         Discover a location that balances convenience with calm — close to top
         schools, malls, and city hubs, yet wrapped in nature's quiet
       </p>
-      <div ref={mapRef} id="nearby" className="map-container" />
+      <div className="left-and-right-padding-in-content">
+        <div
+          ref={mapRef}
+          id="nearby"
+          className="map-container "
+        />
+      </div>
     </div>
   );
 }
