7adcfd7e7417e58a73bc1af2a9481cf837d76e19
Abhinaba Jana <abhinabajana900@gmail.com>
2025-11-18 17:17:33 +0530

    feat: remove SmoothScroller component and related dependencies for improved performance

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AuthContextProvider from "./AuthContext/AuthContext";
 import { Analytics } from "@vercel/analytics/next";
-import SmoothScroller from "@/pages/home/hooks/useLenis";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
